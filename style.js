296f74febeaa5e5f59596377ed7e35a2bca3e31a
dauduchieu <dauduchieu.ddh775@gmail.com>
2025-02-22 14:57:26 +0700

    add utils, style.js, input animation

---
+++ style.js
@@ -0,0 +1,25 @@
+const s_inpFile = $('#inp-file')
+const s_inputLabel = $('.input-file')
+const s_inputIcon = $('.interact .input-file i')
+const s_inputFileName = $('.interact .file-name')
+let isFirstInput = true
+
+function changeAnimation(element, animationString) {
+    element.style.animation = 'none'
+    void element.offsetWidth
+    element.style.animation = animationString
+}
+
+function fileChangeAnimation(e) {
+    if (!isFirstInput) return
+    isFirstInput = false
+
+    const fileName = e.target.files[0]['name']
+    s_inputFileName.innerText = fileName
+    
+    changeAnimation(s_inputLabel, `input-file-on-input 1s linear 1 forwards`)
+    changeAnimation(s_inputIcon, `input-file-icon-on-input 1s linear 1 forwards`)
+    changeAnimation(s_inputFileName, 'input-file-name-on-input 1s linear 1 forwards')
+}
+
+s_inpFile.addEventListener('change', fileChangeAnimation)
